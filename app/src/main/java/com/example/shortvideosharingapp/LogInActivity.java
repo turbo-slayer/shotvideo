04f45e81cc6e3c69a9a91e93806fca8749b0f465
turbo-slayer <55850010+turbo-slayer@users.noreply.github.com>
2021-11-02 23:14:50 +0530

    Added and updated files

---
+++ app/src/main/java/com/example/shortvideosharingapp/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.shortvideosharingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        //creating the object of the textViewButton-signUpTextView
+    }
+}
+
---
+++ app/src/main/java/com/example/shortvideosharingapp/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.shortvideosharingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        //object of the button, which needs data
+        TextView button = findViewById(R.id.alreadyHaveAccount);
+        //settting up the set on click listner
+    }
+}
